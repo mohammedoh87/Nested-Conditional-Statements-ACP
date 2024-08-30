426e51ec2de258d286fce092e0e9563abaa06356
mohammedoh87 <aamina.oh87@gmail.com>
2024-08-30 16:06:27 +0300

    Nested Conditional Statements ACP

---
+++ main2.py
@@ -0,0 +1,13 @@
+age = int(input("How old are you? "))
+
+if age <= 20:
+    if age >= 10:
+        print("You are between 10 and 20")
+    else:
+        print("You are not between 10 and 20")
+
+else:
+    print("WRONG ANSWER")
+
+
+
